6af745afcb62eb7744c7d80b44f1bc86977726cd
CJ Collins <62709862+CJonPhiya@users.noreply.github.com>
2020-04-01 21:01:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,175 @@
+'''
+    Name: PhiyaByte
+    Dev : CJ Collins & Nehal J Wani
+    Tool: Linux Hard-Drive Utility
+    Date: 4/1/2020
+    Version: 0.1
+
+'''
+
+import os
+import sys
+import string
+
+def startmenu():
+
+    print("+++========================================================+++")    
+    print("##############################################################")    
+    print("+++	  ____  _     _             ____        _          +++")
+    print("+++	 |  _ \| |__ (_)_   _  __ _| __ ) _   _| |_ ___    +++")
+    print("+++	 | |_) | '_ \| | | | |/ _` |  _ \| | | | __/ _ \   +++")
+    print("+++	 |  __/| | | | | |_| | (_| | |_) | |_| | ||  __/   +++")
+    print("+++	 |_|   |_| |_|_|\__, |\__,_|____/ \__, |\__\___|   +++") 
+    print("+++				          |___/ Version 0.1+++")
+    print("+++	                                                   +++")
+    print("+++	         By: CJ Collins & Nehal J Wani             +++")
+    print(".-{[                 Hard-disk Utility                    ]}-.")
+    print(".-{[                                                      ]}-.")
+    print("##############################################################")
+    print("+++========================================================+++") 
+    print("")
+    print("+============================================================+")
+    print("0 -                 |")
+    print("1 -                       |")
+    print("2 -          |")
+    print("3 -                      |")
+    print("4 -           |")
+    print("5 -        |")
+    print("6 -                     |")
+    print("7 -                             |")
+    print("8 -       |")
+    print("X - Exit Utility                                          |")    
+    print("+============================================================+")
+    print("")
+    print("")
+    
+startmenu()
+menuchoice = input("Enter your selection :")
+if menuchoice == "0":
+        Selection0()
+elif menuchoice == "1":
+        Selection1()
+elif menuchoice == "2":
+        Selection2()
+elif menuchoice == "3":
+        Selection3()
+elif menuchoice == "4":
+        Selection4()
+elif menuchoice == "5":
+        Selection5()
+elif menuchoice == "6":
+        Selection6()
+elif menuchoice == "7":
+        Selection7()
+elif menuchoice == "8":
+        Selection8()
+elif menuchoice == "X":
+        sys.exit()        
+else:
+        print("Invalid selection! Please try again. Press <ENTER>.")
+        input("")
+        startmenu()
+   
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #0
+'''
+def Selection0():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+    
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #1
+'''
+def Selection1():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+	
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #2
+'''
+def Selection2():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+	
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #3
+'''
+def Selection3():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+	
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #4
+'''
+def Selection4():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+	
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #5
+'''
+def Selection5():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+	
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #5
+'''
+def Selection5():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+	
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #6
+'''
+def Selection6():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+	
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #7
+'''
+def Selection7():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+	
+'''
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    Option #8
+'''
+def Selection8():
+    print("")
+    print("")
+    print(".-{[ Output text here]}-.")
+    startmenu()
+
+
+ 
+ 
